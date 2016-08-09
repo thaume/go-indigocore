6996def5c54a364b6057653ade439370220339f2
Stephan Florquin <stephan.florquin@gmail.com>
2016-08-09 14:08:12 +0200

    JSON http tweaks

--- jsonhttp/jsonhttp.go
+++ jsonhttp/jsonhttp.go
@@ -44,17 +44,19 @@ type Server struct {
 	config *Config
 }
 
-// JSONHandle is the function type for a route handle.
-type JSONHandle func(http.ResponseWriter, *http.Request, httprouter.Params, *Config) (interface{}, error)
+// Handle is the function type for a route handle.
+type Handle func(http.ResponseWriter, *http.Request, httprouter.Params, *Config) (interface{}, error)
 
-// NotFoundHandler is a handler for undefined routes.
-type NotFoundHandler struct {
-	handler
+// NotFound is a handle for a route that is not found.
+func NotFound(w http.ResponseWriter, r *http.Request, _ httprouter.Params, _ *Config) (interface{}, error) {
+	return nil, NewErrNotFound("")
 }
 
-// ServeHTTP implements net/http.Handler.ServeHTTP.
-func (h NotFoundHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.handler.ServeHTTP(w, r, nil)
+// New creates an instance of Server.
+func New(config *Config) *Server {
+	r := httprouter.New()
+	r.NotFound = notFoundHandler{handler{config, NotFound}}
+	return &Server{r, config}
 }
 
 // ServeHTTP implements net/http.Handler.ServeHTTP.
@@ -62,45 +64,38 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
 }
 
-// New creates an instance of Server.
-func New(config *Config) *Server {
-	r := httprouter.New()
-	r.NotFound = NotFoundHandler{handler{config, notFound}}
-	return &Server{r, config}
-}
-
 // Get adds a GET route.
-func (s *Server) Get(path string, handle JSONHandle) {
+func (s *Server) Get(path string, handle Handle) {
 	s.router.GET(path, handler{s.config, handle}.ServeHTTP)
 }
 
 // Post adds a POST route.
-func (s *Server) Post(path string, handle JSONHandle) {
+func (s *Server) Post(path string, handle Handle) {
 	s.router.POST(path, handler{s.config, handle}.ServeHTTP)
 }
 
 // Put adds a PUT route.
-func (s *Server) Put(path string, handle JSONHandle) {
+func (s *Server) Put(path string, handle Handle) {
 	s.router.PUT(path, handler{s.config, handle}.ServeHTTP)
 }
 
 // Delete adds a DELETE route.
-func (s *Server) Delete(path string, handle JSONHandle) {
+func (s *Server) Delete(path string, handle Handle) {
 	s.router.DELETE(path, handler{s.config, handle}.ServeHTTP)
 }
 
 // Patch adds a PATCH route.
-func (s *Server) Patch(path string, handle JSONHandle) {
+func (s *Server) Patch(path string, handle Handle) {
 	s.router.PATCH(path, handler{s.config, handle}.ServeHTTP)
 }
 
 // Head adds a HEAD route.
-func (s *Server) Head(path string, handle JSONHandle) {
+func (s *Server) Head(path string, handle Handle) {
 	s.router.HEAD(path, handler{s.config, handle}.ServeHTTP)
 }
 
 // Options adds an OPTIONS route.
-func (s *Server) Options(path string, handle JSONHandle) {
+func (s *Server) Options(path string, handle Handle) {
 	s.router.OPTIONS(path, handler{s.config, handle}.ServeHTTP)
 }
 
@@ -119,13 +114,9 @@ func (s *Server) ListenAndServe() error {
 	return http.ListenAndServe(p, s)
 }
 
-func notFound(w http.ResponseWriter, r *http.Request, _ httprouter.Params, _ *Config) (interface{}, error) {
-	return nil, NewErrNotFound("")
-}
-
 type handler struct {
 	config *Config
-	serve  JSONHandle
+	serve  Handle
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -163,4 +154,12 @@ func renderErr(w http.ResponseWriter, err error, c *Config) {
 
 	w.Header().Set("Content-Type", "application/json")
 	http.Error(w, string(js), e.Status())
+}
+
+type notFoundHandler struct {
+	handler
+}
+
+func (h notFoundHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.handler.ServeHTTP(w, r, nil)
 }
