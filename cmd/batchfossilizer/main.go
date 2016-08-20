8e4cfa1158374fc68dbdfe21fd26fe61974633bf
Stephan Florquin <stephan.florquin@gmail.com>
2016-08-20 22:04:47 +0200

    cmd: Fix batchfossilizer log prefix

---
+++ cmd/batchfossilizer/main.go
@@ -0,0 +1,57 @@
+// Copyright 2016 Stratumn SAS. All rights reserved.
+// Use of this source code is governed by an Apache License 2.0
+// that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/stratumn/go/fossilizer/fossilizerhttp"
+	"github.com/stratumn/go/jsonhttp"
+	"github.com/stratumn/goprivate/batchfossilizer"
+	"github.com/stratumn/goprivate/merkle"
+)
+
+var (
+	port             = flag.String("port", fossilizerhttp.DefaultPort, "server port")
+	certFile         = flag.String("tlscert", "", "TLS certificate file")
+	keyFile          = flag.String("tlskey", "", "TLS private key file")
+	numResultWorkers = flag.Int("workers", fossilizerhttp.DefaultNumResultWorkers, "number of result workers")
+	callbackTimeout  = flag.Duration("callbacktimeout", fossilizerhttp.DefaultCallbackTimeout, "callback requests timeout")
+	verbose          = flag.Bool("verbose", fossilizerhttp.DefaultVerbose, "verbose output")
+	interval         = flag.Duration("interval", batchfossilizer.DefaultInterval, "batch interval")
+	maxLeaves        = flag.Int("maxleaves", batchfossilizer.DefaultMaxLeaves, "maximum number of leaves in a Merkle tree")
+	version          = ""
+)
+
+func init() {
+	log.SetPrefix("batchfossilizer ")
+}
+
+func main() {
+	flag.Parse()
+
+	a := batchfossilizer.New(&batchfossilizer.Config{
+		Version:   version,
+		Interval:  *interval,
+		MaxLeaves: *maxLeaves,
+	})
+	c := &fossilizerhttp.Config{
+		Config: jsonhttp.Config{
+			Port:     *port,
+			CertFile: *certFile,
+			KeyFile:  *keyFile,
+			Verbose:  *verbose,
+		},
+		NumResultWorkers: *numResultWorkers,
+		CallbackTimeout:  *callbackTimeout,
+		MinDataLen:       merkle.HashByteLen,
+		MaxDataLen:       merkle.HashByteLen,
+	}
+	h := fossilizerhttp.New(a, c)
+
+	log.Printf("Listening on %s", *port)
+	log.Fatal(h.ListenAndServe())
+}
