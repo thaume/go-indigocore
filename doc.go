1d7dec302a4fe9ad704c4c6efc6dbf01044e3020
Stephan Florquin <stephan.florquin@gmail.com>
2016-08-30 00:03:03 +0200

    License

--- doc.go
+++ doc.go
@@ -14,10 +14,19 @@
 
 // Package stratumngo contains open source Go packages and tools for Stratumn.
 //
-// Copyright 2016 Stratumn SAS.
+// Copyright 2016 Stratumn SAS. All rights reserved.
 //
-// Unless otherwise noted, the Stratumn Go source files are distributed
-// under the Apache License 2.0 found in the LICENSE file.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 // Third party dependencies included in the vendor directory are distributed
 // under their respective licenses.
