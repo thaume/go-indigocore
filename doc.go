9cf382e5641f773215bbeacb911571ca0ec3ddb8
stephan <stephan83@users.noreply.github.com>
2017-02-15 18:56:34 +0100

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,10 +1,14 @@
-// Package stratumngo contains open source Go packages and tools for Stratumn.
-//
 // Copyright 2017 Stratumn SAS. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// Stratumn's SDK to create Indigo applications and networks.
+//
+// Unless otherwise noted, the Stratumn Agent Javascript Library source files
+// are distributed under the Mozilla Public License 2.0 found in the LICENSE
+// file.
 //
 // Third party dependencies included in the vendor directory are distributed
 // under their respective licenses.
