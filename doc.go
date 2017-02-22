e883b8952b5f2e0f27d8ba2278315b44994c8eb7
Stephan Florquin <stephan.florquin@gmail.com>
2017-02-22 15:04:26 +0100

    fix SDK doc

--- doc.go
+++ doc.go
@@ -4,11 +4,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// Stratumn's SDK to create Indigo applications and networks.
+// Package sdk contains Stratumn tools and functionality to create Indigo
+// applications and networks.
 //
 // Unless otherwise noted, the source files are distributed under the Mozilla
 // Public License 2.0 found in the LICENSE file.
 //
 // Third party dependencies included in the vendor directory are distributed
 // under their respective licenses.
-package stratumngo
+package sdk
