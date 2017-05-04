102308b3df409abcef292590e44dd1d3610e5878
Adrien Montfort <adrien.montfort@gmail.com>
2017-05-04 17:36:43 +0200

    cmd: Cleanup flags
    - use cleanup from sdk

--- cmd/postgresstore/main.go
+++ cmd/postgresstore/main.go
@@ -2,125 +2,33 @@
 // Use of this source code is governed by an Apache License 2.0
 // LICENSE file.
 
+// The command postgresstore starts an HTTP server with a postgresstore.
 package main
 
 import (
 	"flag"
-	"fmt"
-	"os"
-	"runtime"
-	"time"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/lib/pq"
 
-	"github.com/stratumn/sdk/jsonhttp"
-	"github.com/stratumn/sdk/jsonws"
-	"github.com/stratumn/sdk/store/storehttp"
 	"github.com/stratumn/goprivate/postgresstore"
+	"github.com/stratumn/sdk/store/storehttp"
 )
-
-const (
-	connectAttempts = 12
-	connectTimeout  = 10 * time.Second
-	noTableCode     = pq.ErrorCode("42P01")
-)
-
-func orStrings(strs ...string) string {
-	for _, s := range strs {
-		if s != "" {
-			return s
-		}
-	}
-	return ""
-}
 
 var (
-	create          = flag.Bool("create", false, "create tables and indexes then exit")
-	drop            = flag.Bool("drop", false, "drop tables and indexes then exit")
-	url             = flag.String("url", orStrings(os.Getenv("POSTGRESSTORE_URL"), postgresstore.DefaultURL), "URL of the PostgreSQL database")
-	http            = flag.String("http", storehttp.DefaultAddress, "HTTP address")
-	wsReadBufSize   = flag.Int("wsreadbufsize", storehttp.DefaultWebSocketReadBufferSize, "Web socket read buffer size")
-	wsWriteBufSize  = flag.Int("wswritebufsize", storehttp.DefaultWebSocketWriteBufferSize, "Web socket write buffer size")
-	wsWriteChanSize = flag.Int("wswritechansize", storehttp.DefaultWebSocketWriteChanSize, "Size of a web socket connection write channel")
-	wsWriteTimeout  = flag.Duration("wswritetimeout", storehttp.DefaultWebSocketWriteTimeout, "Timeout for a web socket write")
-	wsPongTimeout   = flag.Duration("wspongtimeout", storehttp.DefaultWebSocketPongTimeout, "Timeout for a web socket expected pong")
-	wsPingInterval  = flag.Duration("wspinginterval", storehttp.DefaultWebSocketPingInterval, "Interval between web socket pings")
-	wsMaxMsgSize    = flag.Int64("maxmsgsize", storehttp.DefaultWebSocketMaxMsgSize, "Maximum size of a received web socket message")
-	certFile        = flag.String("tlscert", "", "TLS certificate file")
-	keyFile         = flag.String("tlskey", "", "TLS private key file")
-	version         = "0.1.0"
-	commit          = "00000000000000000000000000000000"
+	version = "0.1.0"
+	commit  = "00000000000000000000000000000000"
 )
+
+func init() {
+	storehttp.RegisterFlags()
+	postgresstore.RegisterFlags()
+}
 
 func main() {
 	flag.Parse()
 
 	log.Infof("%s v%s@%s", postgresstore.Description, version, commit[:7])
-	log.Info("Copyright (c) 2016 Stratumn SAS")
-	log.Info("All Rights Reserved")
-	log.Infof("Runtime %s %s %s", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 
-	a, err := postgresstore.New(&postgresstore.Config{URL: *url, Version: version, Commit: commit})
-	if err != nil {
-		log.WithField("error", err).Fatal("Failed to create PostgreSQL store")
-	}
-
-	if *create {
-		if err := a.Create(); err != nil {
-			log.WithField("error", err).Fatal("Failed to create PostgreSQL tables and indexes")
-		}
-		log.Info("Created tables and indexes")
-		os.Exit(0)
-	}
-
-	if *drop {
-		if err := a.Drop(); err != nil {
-			log.WithField("error", err).Fatal("Failed to drop PostgreSQL tables and indexes")
-		}
-		log.Info("Dropped tables and indexes")
-		os.Exit(0)
-	}
-
-	for i := 1; i <= connectAttempts; i++ {
-		if err != nil {
-			time.Sleep(connectTimeout)
-		}
-		if err = a.Prepare(); err != nil {
-			if e, ok := err.(*pq.Error); ok && e.Code == noTableCode {
-				if err = a.Create(); err != nil {
-					log.WithField("error", err).Fatal("Failed to create PostgreSQL tables and indexes")
-				}
-				log.Info("Created tables and indexes")
-			} else {
-				log.WithFields(log.Fields{
-					"attempt": i,
-					"max":     connectAttempts,
-				}).Warn(fmt.Sprintf("Unable to connect to PostgreSQL, retrying in %v", connectTimeout))
-			}
-		} else {
-			break
-		}
-	}
-	if err != nil {
-		log.WithField("max", connectAttempts).Fatal("Unable to connect to PostgreSQL")
-	}
-
-	httpConfig := &jsonhttp.Config{
-		Address:  *http,
-		CertFile: *certFile,
-		KeyFile:  *keyFile,
-	}
-	basicConfig := &jsonws.BasicConfig{
-		ReadBufferSize:  *wsReadBufSize,
-		WriteBufferSize: *wsWriteBufSize,
-	}
-	bufConnConfig := &jsonws.BufferedConnConfig{
-		Size:         *wsWriteChanSize,
-		WriteTimeout: *wsWriteTimeout,
-		PongTimeout:  *wsPongTimeout,
-		PingInterval: *wsPingInterval,
-		MaxMsgSize:   *wsMaxMsgSize,
-	}
-	storehttp.Run(a, httpConfig, basicConfig, bufConnConfig)
+	a := postgresstore.InitializeWithFlags(version, commit)
+	storehttp.RunWithFlags(a)
 }
--- cmd/rethinkstore/main.go
+++ cmd/rethinkstore/main.go
@@ -2,132 +2,34 @@
 // Use of this source code is governed by an Apache License 2.0
 // LICENSE file.
 
+// The command rethinkstore starts an HTTP server with a rethinkstore.
 package main
 
 import (
 	"flag"
-	"fmt"
-	"os"
-	"runtime"
-	"time"
 
 	log "github.com/Sirupsen/logrus"
 
-	"github.com/stratumn/sdk/jsonhttp"
-	"github.com/stratumn/sdk/jsonws"
 	"github.com/stratumn/sdk/store/storehttp"
 
 	"github.com/stratumn/goprivate/rethinkstore"
 )
 
-const (
-	connectAttempts = 12
-	connectTimeout  = 10 * time.Second
-)
-
-func orStrings(strs ...string) string {
-	for _, s := range strs {
-		if s != "" {
-			return s
-		}
-	}
-	return ""
-}
-
 var (
-	create          = flag.Bool("create", false, "create tables and indexes then exit")
-	drop            = flag.Bool("drop", false, "drop tables and indexes then exit")
-	url             = flag.String("url", orStrings(os.Getenv("RETHINKSTORE_URL"), rethinkstore.DefaultURL), "URL of the RethinkDB database")
-	db              = flag.String("db", orStrings(os.Getenv("RETHINKSTORE_DB"), rethinkstore.DefaultDB), "name of the RethinkDB database")
-	hard            = flag.Bool("hard", rethinkstore.DefaultHard, "whether to use hard durability")
-	http            = flag.String("http", storehttp.DefaultAddress, "HTTP address")
-	wsReadBufSize   = flag.Int("wsreadbufsize", storehttp.DefaultWebSocketReadBufferSize, "Web socket read buffer size")
-	wsWriteBufSize  = flag.Int("wswritebufsize", storehttp.DefaultWebSocketWriteBufferSize, "Web socket write buffer size")
-	wsWriteChanSize = flag.Int("wswritechansize", storehttp.DefaultWebSocketWriteChanSize, "Size of a web socket connection write channel")
-	wsWriteTimeout  = flag.Duration("wswritetimeout", storehttp.DefaultWebSocketWriteTimeout, "Timeout for a web socket write")
-	wsPongTimeout   = flag.Duration("wspongtimeout", storehttp.DefaultWebSocketPongTimeout, "Timeout for a web socket expected pong")
-	wsPingInterval  = flag.Duration("wspinginterval", storehttp.DefaultWebSocketPingInterval, "Interval between web socket pings")
-	wsMaxMsgSize    = flag.Int64("maxmsgsize", storehttp.DefaultWebSocketMaxMsgSize, "Maximum size of a received web socket message")
-	certFile        = flag.String("tlscert", "", "TLS certificate file")
-	keyFile         = flag.String("tlskey", "", "TLS private key file")
-	version         = "0.1.0"
-	commit          = "00000000000000000000000000000000"
+	version = "0.1.0"
+	commit  = "00000000000000000000000000000000"
 )
+
+func init() {
+	storehttp.RegisterFlags()
+	rethinkstore.RegisterFlags()
+}
 
 func main() {
 	flag.Parse()
 
 	log.Infof("%s v%s@%s", rethinkstore.Description, version, commit[:7])
-	log.Info("Copyright (c) 2016 Stratumn SAS")
-	log.Info("All Rights Reserved")
-	log.Infof("Runtime %s %s %s", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 
-	a, err := rethinkstore.New(&rethinkstore.Config{
-		URL:     *url,
-		DB:      *db,
-		Hard:    *hard,
-		Version: version,
-		Commit:  commit,
-	})
-	if err != nil {
-		log.WithField("error", err).Fatal("Failed to create RethinkDB store")
-	}
-
-	if *create {
-		if err := a.Create(); err != nil {
-			log.Fatalf("Fatal: %s", err)
-		}
-		log.WithField("error", err).Fatal("Failed to create RethinkDB tables and indexes")
-		os.Exit(0)
-	}
-
-	if *drop {
-		if err := a.Drop(); err != nil {
-			log.WithField("error", err).Fatal("Failed to drop RethinkDB tables and indexes")
-		}
-		log.Info("Dropped tables and indexes")
-		os.Exit(0)
-	}
-
-	exists := false
-	for i := 1; i <= connectAttempts; i++ {
-		if err != nil {
-			time.Sleep(connectTimeout)
-		}
-		if exists, err = a.Exists(); err != nil {
-			log.WithFields(log.Fields{
-				"attempt": i,
-				"max":     connectAttempts,
-			}).Warn(fmt.Sprintf("Unable to connect to RethinkDB, retrying in %v", connectTimeout))
-		} else {
-			if !exists {
-				if err = a.Create(); err != nil {
-					log.WithField("error", err).Fatal("Failed to create RethinkDB tables and indexes")
-				}
-				log.Info("Created tables and indexes")
-			}
-			break
-		}
-	}
-	if err != nil {
-		log.WithField("max", connectAttempts).Fatal("Unable to connect to RethinkDB")
-	}
-
-	httpConfig := &jsonhttp.Config{
-		Address:  *http,
-		CertFile: *certFile,
-		KeyFile:  *keyFile,
-	}
-	basicConfig := &jsonws.BasicConfig{
-		ReadBufferSize:  *wsReadBufSize,
-		WriteBufferSize: *wsWriteBufSize,
-	}
-	bufConnConfig := &jsonws.BufferedConnConfig{
-		Size:         *wsWriteChanSize,
-		WriteTimeout: *wsWriteTimeout,
-		PongTimeout:  *wsPongTimeout,
-		PingInterval: *wsPingInterval,
-		MaxMsgSize:   *wsMaxMsgSize,
-	}
-	storehttp.Run(a, httpConfig, basicConfig, bufConnConfig)
+	a := rethinkstore.InitializeWithFlags(version, commit)
+	storehttp.RunWithFlags(a)
 }
