2a29997374862741c774de23989ab3ac3a9cdc7a
Adrien Montfort <adrien.montfort@gmail.com>
2017-11-16 14:30:32 +0100

    cmd: rethinkstore: fix cmd
    - Properly initialize tendermint
    - Add validation file option

--- cmd/rethinktmpop/main.go
+++ cmd/rethinktmpop/main.go
@@ -11,16 +11,20 @@ import (
 	"flag"
 
 	"github.com/stratumn/goprivate/rethinkstore"
+	"github.com/stratumn/sdk/tendermint"
 	"github.com/stratumn/sdk/tmpop"
+	"github.com/stratumn/sdk/validator"
 )
 
 var (
-	cacheSize = flag.Int("cacheSize", tmpop.DefaultCacheSize, "size of the cache of the storage tree")
-	version   = "0.1.0"
-	commit    = "00000000000000000000000000000000"
+	cacheSize         = flag.Int("cacheSize", tmpop.DefaultCacheSize, "size of the cache of the storage tree")
+	validatorFilename = flag.String("rules_filename", validator.DefaultFilename, "Path to filename containing validation rules")
+	version           = "0.1.0"
+	commit            = "00000000000000000000000000000000"
 )
 
 func init() {
+	tendermint.RegisterFlags()
 	rethinkstore.RegisterFlags()
 }
 
@@ -29,7 +33,7 @@ func main() {
 
 	a := rethinkstore.InitializeWithFlags(version, commit)
 
-	tmpopConfig := &tmpop.Config{Commit: commit, Version: version, CacheSize: *cacheSize}
+	tmpopConfig := &tmpop.Config{Commit: commit, Version: version, CacheSize: *cacheSize, ValidatorFilename: *validatorFilename}
 
 	tmpop.Run(a, tmpopConfig)
 }
